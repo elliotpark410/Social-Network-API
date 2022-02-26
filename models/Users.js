4909437f3da3e5a9c4cf085896e17c4ddada3ad4
Elliot Park <elliotpark410@gmail.com>
2022-02-25 23:33:48 -0800

    create user model

---
+++ models/Users.js
@@ -0,0 +1,56 @@
+const { Schema, model } = require('mongoose');
+
+// Schema to create User model
+const userSchema = new Schema(
+  // define properties
+  {
+    username: {
+      type: String, 
+      unique: true,
+      required: true,
+      trim: true
+    },
+    email: {
+      type: String, 
+      unique: true,
+      required: true,
+      // match email address with regular expression to validate email address
+      match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+    },
+    // populating subdocuments Thoughts
+    Thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Thoughts',
+      },
+    ],
+    // populating subdocument Friends
+    friends: [
+      {
+      type: Schema.Types.ObjectId,
+      ref: 'Users'
+      }
+    ],
+  },
+  {
+    // Mongoose supports transforming Objects after querying MongoDb: toJSON 
+    // Here we are indicating that we want virtuals to be included with our response, overriding the default behavior
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+// Create a virtual property `friendCount` that gets the user's friends array length
+userSchema
+  .virtual('friendCount')
+  // Getter
+  .get(function () {
+    return this.friends.length;
+  });
+
+// Initialize our User model
+const Users = model('Users', userSchema);
+
+module.exports = Users;
